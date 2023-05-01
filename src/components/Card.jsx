a8904b01b9f281f8745d2f4d917a397db844fe63
rstott87 <rstott87@yahoo.com>
2023-05-01 15:24:55 -0400

    update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 function Card(props) {
     return (
-      <div className=" p-6 mb-6 shadow-lg shadow-slate-600 flex flex-col gap-4 text-slate-300 bg-slate-700 rounded-lg border-2 text-center border-slate-900">
+      <div className=" p-6 mb-6 shadow-lg shadow-slate-600 flex flex-col gap-3 text-slate-300 bg-slate-700 rounded-lg border-2 text-center border-slate-900">
         <p className="text-4xl">{props.title}</p>
         <div className="flex justify-center">
           <img
@@ -12,7 +12,10 @@ function Card(props) {
             alt={""}
           />
         </div>
-        <p className="text-2xl">Views: {props.viewCount}</p>
+        <div>
+            <p className="text-2xl">Views: {props.viewCount}</p>
+            <p className="text-2xl">Videos: {props.videoCount}</p>
+        </div>
       </div>
     );
 }
--- src/pages/axiostest.js
+++ src/pages/axiostest.js
@@ -12,7 +12,14 @@ export default function FetchParent() {
   const [searchResponse, setSearchResponse] = useState(channelSchema.items);
   
     const listOfPodcasts = searchResponse.map((item) => (
-      <Card key={item.id} className="text-2xl" title={item.snippet.title} viewCount={item.statistics.viewCount} thumbnail={item.snippet.thumbnails.medium.url}/>
+      <Card
+        key={item.id}
+        className="text-2xl"
+        title={item.snippet.title}
+        videoCount={item.statistics.videoCount}
+        viewCount={item.statistics.viewCount}
+        thumbnail={item.snippet.thumbnails.medium.url}
+      />
     ));
     console.log(listOfPodcasts);
   
