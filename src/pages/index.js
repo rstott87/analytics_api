a7182cdb79ac89d28e550b6366ea62cea023ea22
rstott87 <rstott87@yahoo.com>
2023-05-30 18:48:12 -0400

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-evenly p-24">
       <h1 className="text-6xl font-bold text-center">
         {" "}
-        Welcome to PodTube by FCR Analytics!
+        Welcome to my Insights by FCR Analytics!
       </h1>
 
       <Link href="/channel-search">
