8f5cb9a01599a8061e296d3aeb35dd9320fe601f
rstott87 <rstott87@yahoo.com>
2023-06-15 20:18:31 -0400

    made some changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,15 +9,27 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-evenly p-24">
-      <h1 className="text-6xl font-bold text-center">
+    <main className="bg-black flex min-h-screen flex-col items-center justify-evenly p-6">
+      <div className="text-6xl text-neutral-100 font-extrabold flex flex-col gap-4">
         {" "}
-        Welcome to my Insights by FCR Analytics!
-      </h1>
+        <div className="p-4">
+          <p>Fast.</p>
+          <p>Clear.</p>
+          <p>Real-Time.</p>
+        </div>
+        <div>
+          <p className="text-2xl font-light text-slate-400 text-center leading-8">
+            {" "}
+            An intuitive and user-friendly platform that allows
+            you to access detailed data and insightful analytics for any YouTube
+            channel.
+          </p>
+        </div>
+      </div>
 
       <Link href="/channel-search">
         {" "}
-        <h1 className="shadow-lg text-3xl bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <h1 className="shadow-lg text-2xl bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">
           Get Started
         </h1>
       </Link>
