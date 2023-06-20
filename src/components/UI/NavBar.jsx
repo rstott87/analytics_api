2f0e354edfafe25ed76255993340dd4492c7e9f2
rstott87 <rstott87@yahoo.com>
2023-06-20 13:04:20 -0400

    styling changes

--- src/components/UI/NavBar.jsx
+++ src/components/UI/NavBar.jsx
@@ -2,7 +2,7 @@ import HambugerMenu from "./HamburgerMenu";
 
 function NavBar(props) {
   return (
-    <div className="space-between nav-bar sticky top-0 z-50 flex justify-between border-none bg-black p-4 pl-8 text-2xl font-normal text-neutral-300 opacity-90 shadow-md shadow-slate-600">
+    <div className="space-between nav-bar sticky top-0 z-50 flex justify-between border-none bg-black p-4 pl-8 text-xl font-extrabold text-neutral-300 shadow-md">
       <p>FCR Analytics</p>
       <HambugerMenu />
     </div>
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,9 +2,9 @@ import NavBar from "./UI/NavBar";
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div className="bg-black">
       <NavBar />
       <main>{children}</main>
-    </>
+    </div>
   );
 }
