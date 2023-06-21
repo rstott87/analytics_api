61e4038ba77a32e50136c4d23f9794780e43a095
rstott87 <rstott87@yahoo.com>
2023-06-21 19:17:15 -0400

    change

--- src/components/StatCard.jsx
+++ src/components/StatCard.jsx
@@ -3,11 +3,11 @@ function StatCard(props) {
 
   return (
     <div className="flex flex-col gap-1 rounded-md border-2 border-slate-200 bg-slate-100 p-4 text-2xl text-gray-800 shadow-md md:text-lg">
-      <p className=" text-left font-medium text-xl">{props.title}</p>
+      <h2 className=" text-left font-medium text-xl">{props.title}</h2>
       <div className="text-xl items-center flex justify-between gap-4">
-        <p className="text-left font-semibold text-violet-800 md:text-lg">
+        <h3 className="text-left font-semibold text-violet-800 md:text-lg">
           {count}
-        </p>
+        </h3>
         <p className="text-lg rounded-full min-w-max bg-emerald-300 border-1 border-green-900 px-2 text-slate-600">
           ^ {props.percent}%
         </p>
