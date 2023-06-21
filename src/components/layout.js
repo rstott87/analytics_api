3ef6f2e8b6b34bc850fe9bb3a48bc5f5368bc70c
rstott87 <rstott87@yahoo.com>
2023-06-21 17:58:57 -0400

    updated URL

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,10 +2,10 @@ import NavBar from "./UI/NavBar";
 
 export default function Layout({ children }) {
   return (
-    <div className="bg-black">
+    <>
       <NavBar />
       <main>{children}</main>
-      <footer className="text-slate-500 text-center text-sm p-4 pt-8"> FCR Analytics LLC 2023 </footer>
-    </div>
+      <footer className="text-slate-500 text-center text-sm p-4 pt-8"> FCR AnalyticsLLC 2023 </footer>
+    </>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,8 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center bg-black px-8 py-12 pt-32 text-center">
       <Head>
-        <title>StatCrown: YouTube Analytics Simplified</title>
+        <title>StatCrown: YouTube Stats and Analytics Simplified</title>
+        <meta name="description" content="A new way to get youtube stats and analytics with simple, to easy understand charts and visualizationsnp" key="desc"></meta>
       </Head>
       <section className="max-w-md pb-6 text-6xl ">
         <div className="px-4 text-5xl font-bold  text-neutral-300 ">
@@ -27,7 +28,7 @@ export default function Home() {
           channel in seconds.
         </h3>
 
-        <Link href="/channel-search">
+        <Link href="/youtube-channel-search">
           <p className="rounded-full bg-neutral-200 px-4 py-2 text-xl font-normal text-slate-900 shadow-md shadow-purple-500">
             {`Get Started`}
           </p>
