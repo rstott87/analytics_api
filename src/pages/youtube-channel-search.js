2046f70bec5d6dfc3e67f792f4b9a17d7b4cf98d
rstott87 <rstott87@yahoo.com>
2023-06-21 18:03:28 -0400

    added more mata data

--- src/pages/youtube-channel-search.js
+++ src/pages/youtube-channel-search.js
@@ -4,6 +4,7 @@ import channelSchema from "../data/channelSchema";
 import SearchForm from "../components/SearchForm";
 import ChannelCard from "../components/ChannelCard";
 import CardDisplay from "../components/CardDisplay";
+import Head from "next/head";
 
 const baseURL =
   "https://www.googleapis.com/youtube/v3/channels?id=UC4fZeoNxAXfbIpT3swsVh9w&part=statistics";
@@ -25,11 +26,17 @@ export default function ChannelSearch() {
     ));
 
   return (
-    <div className="bg-slate-100 flex flex-col items-center">
-      <SearchForm
-        setSearchResponse={setSearchResponse}
-      />
-      <CardDisplay listOfPodcastCards={listOfPodcastCards}/>
+    <div className="flex flex-col items-center bg-slate-100">
+      <Head>
+        <title>StatCrown: gSearch for Any Youtube Channel to Return Stats</title>
+        <meta
+          name="description"
+          content="The search form to find stats on any youtube channel"
+          key="desc"
+        ></meta>
+      </Head>
+      <SearchForm setSearchResponse={setSearchResponse} />
+      <CardDisplay listOfPodcastCards={listOfPodcastCards} />
     </div>
   );
 }
