500b6c61c16fe774798ad4034f55975b38360e88
rstott87 <rstott87@yahoo.com>
2023-07-02 17:09:25 -0400

    debugging

--- src/pages/channel/[id].js
+++ src/pages/channel/[id].js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 import { useRouter } from "next/router";
 
 export const getServerSideProps = async (context) => {
@@ -23,9 +24,9 @@ export const getServerSideProps = async (context) => {
 };
 
 export default function Channel({ data }) {
-  console.log(data);
   const title = data.items[0].snippet.title;
   const description = data.items[0].snippet.description;
+  const image = data.items[0].snippet.thumbnails.default.url;
   return (
     <div className="_container items-center gap-4 bg-slate-100 p-4">
       <Head>
@@ -36,6 +37,7 @@ export default function Channel({ data }) {
           key="desc"
         ></meta>
       </Head>
+      <Image src={image} width={50} height={50} alt="image of podcast"/>
       <div> {title}</div>
       <div> {description}</div>
     </div>
