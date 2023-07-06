293c02df0119869857f4bb91a74feaa441fc63c7
rstott87 <rstott87@yahoo.com>
2023-07-06 18:24:59 -0400

    changes

--- src/pages/channel/[id].js
+++ src/pages/channel/[id].js
@@ -13,13 +13,10 @@ export const getServerSideProps = async (context) => {
   let playlistId = await dataChannels.items[0].contentDetails.relatedPlaylists
     .uploads;
 
-
-
   const resPlaylistItems = await fetch(
     `https://www.googleapis.com/youtube/v3/playlistItems?playlistId=${playlistId}&key=${key}&part=snippet`
   );
   const dataPlayList = await resPlaylistItems.json();
-
 
   let videoIdArray = await dataPlayList.items.map(
     (item) => item.snippet.resourceId.videoId
