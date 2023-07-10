262d3a21ec9b385b1a082e68f8104a2694ec9103
rstott87 <rstott87@yahoo.com>
2023-07-10 16:57:44 -0400

    changes to how initial data loads

--- src/pages/youtube-channel-search.js
+++ src/pages/youtube-channel-search.js
@@ -1,19 +1,29 @@
 import axios from "axios";
 import Head from "next/head";
 import { useState, useEffect } from "react";
-import channelSchema from "../data/channelSchema";
 import SearchForm from "../components/SearchForm";
 import ChannelCard from "../components/ChannelCard";
 import CardDisplay from "../components/CardDisplay";
 import ErrorMessage from "../components/ErrorMessage";
 
-const baseURL =
-  "https://www.googleapis.com/youtube/v3/channels?id=UC4fZeoNxAXfbIpT3swsVh9w&part=statistics";
 
-export default function ChannelSearch() {
-  // const [searchResponse, setSearchResponse] = useState(channelSchema.items);
 
-  const defaultList = channelSchema.items;
+export const getStaticProps = async () => {
+ const key = process.env.YOUTUBE_API_KEY;
+ const arrayOfChannelIds = [ "UCzQUP1qoWDoEbmsQxvdjxgQ", "UC4fZeoNxAXfbIpT3swsVh9w", "UCIyIoM_Nd8HtY19fuR_ov2A", "UCy6A9WMN43DrtBkID7nMXJw", "UCSHZKyawb77ixDdsGog4iWA", "UCVCiKCwhFCZEnx2lk-FS7lA", "UC15XVjBZTbV1oBebwDY9UBg", "UCeYSlCaX1PheCaqxN7uQX4A", "UC5AQEUAwCh1sGDvkQtkDWUQ"]
+  const arrayOfChannelIdsString = arrayOfChannelIds.join(",");
+  
+
+  const res = await fetch(
+    `https://www.googleapis.com/youtube/v3/channels?id=${arrayOfChannelIdsString}&part=snippet&key=${key}`
+  );
+  const channelData = await res.json();
+  return { props: { channelData } };
+};
+
+export default function ChannelSearch({ channelData }) {
+  console.log(channelData);
+  const defaultList = channelData.items;
 
   const listOfPodcastCards = defaultList ? (
     defaultList.map((item) => (
@@ -29,14 +39,8 @@ export default function ChannelSearch() {
   );
 
   return (
-    <div className="_container flex flex-col gap-4 p-4 items-center bg-slate-100">
+    <div className="_container flex flex-col items-center gap-4 bg-slate-100 p-4">
       <Head>
-        {/* <link
-          rel="icon"
-          href="../images/icon.ico"
-          type="image/svg+xml"
-          sizes="800x800"
-        ></link> */}
         <title>
           YouTube Channel Stats Search Tool | Analyze and Track Video Metrics
         </title>
